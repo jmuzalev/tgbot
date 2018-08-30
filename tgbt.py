7e66132fe965536bfa961767f02020a62292ae6d
jmuzalev <jmuzalev@gmail.com>
2018-08-31 01:36:31 +0300

    scan bot chat in real time and print text

--- tgbt.py
+++ tgbt.py
@@ -1,11 +1,27 @@
 import telepot
+from pprint import pprint
 
-telepot.api.set_proxy("https://181.15.155.245:80")
+telepot.api.set_proxy("https://186.200.35.37:3128")
 bot = telepot.Bot('684539582:AAFYcidGBScltH_nd07xpAIwimIDJ4SndLI')
 
-print(bot.getMe())
+pprint(bot.getMe())
+upd=0
+# print(bot.getUpdates(offset=upd))
+
+# offset=939343314
+while(True):
+    s=bot.getUpdates(offset=upd)
+    if len(s) > 0:
+        print(s)
+        print(s[0].get('message').get('text'))
+        upd = s[-1]['update_id'] +1
+            
+            
 
 
+
+# [{u'message': {u'date': 1459927254, u'text': u'/start', u'from': {u'username': u'adilkhash', u'first_name': u'Adil', u'id': 31337}, u'message_id': 1, u'chat': {u'username': u'adilkhash', u'first_name': u'Adil', u'type': u'private', u'id': 7350}}, u'update_id': 649179764}]
+# bot.sendMessage(176748136,'Hey!')
 
 
 
